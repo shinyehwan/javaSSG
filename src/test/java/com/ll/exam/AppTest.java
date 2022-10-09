749ec5e794c528bd66d47ccd84e63bc7b412de69
shinyehwan <jghisjl007@naver.com>
2022-10-10 00:27:19 +0900

    JUNIT이 잘 작동하는지 테스트

---
+++ src/test/java/com/ll/exam/AppTest.java
@@ -0,0 +1,14 @@
+package com.ll.exam;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class AppTest {
+    @Test
+    public void 테스트_실험() {
+        int rs = 10 + 20;
+        assertEquals(30, rs);
+    }
+}
