2d4257af0a6664dab361ac4b3a380b9b9b9d4a23
shinyehwan <jghisjl007@naver.com>
2022-10-15 01:17:07 +0900

    WiseSayingController에서 본인의 역할에 맞는 일들만 하고 나머지는 하위기관에 위임

--- src/main/java/com/ll/exam/WiseSayingRepository.java
+++ src/main/java/com/ll/exam/WiseSayingRepository.java
@@ -5,9 +5,9 @@ import java.util.List;
 import java.util.Scanner;
 
 public class WiseSayingRepository {
-    public List<WiseSaying> wiseSayings;
+    private List<WiseSaying> wiseSayings;
     // 가장 마지막 명언 글의 번호
-    public int wiseSayingLastId;
+    private int wiseSayingLastId;
 
     WiseSayingRepository() {
         wiseSayings = new ArrayList<>();
@@ -21,6 +21,43 @@ public class WiseSayingRepository {
                 return wiseSaying;
             }
         }
+
+        // 하나 찾기
+
         return null;
+    }
+
+    public List<WiseSaying> findAll() {
+
+        // 모두 찾기
+
+        return wiseSayings;
+    }
+
+    public WiseSaying write(String content, String author) {
+        int id = ++wiseSayingLastId;
+        WiseSaying wiseSaying = new WiseSaying(id, content, author);
+        wiseSayings.add(wiseSaying);
+
+        // 파일 저장
+
+        return wiseSaying;
+    }
+
+    public void remove(int paramId) {
+        WiseSaying foundWiseSaying = findById(paramId);
+        wiseSayings.remove(foundWiseSaying);
+
+        // 파일 삭제
+
+    }
+
+    public void modify(int paramId, String content, String author) {
+        WiseSaying foundWiseSaying = findById(paramId);
+        foundWiseSaying.content = content;
+        foundWiseSaying.author = author;
+
+        // 파일 수정
+
     }
 }
