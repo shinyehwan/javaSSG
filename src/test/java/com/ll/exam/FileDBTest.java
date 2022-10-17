546015db3df1df182d8157183680634e7386cb6d
shinyehwan <jghisjl007@naver.com>
2022-10-17 16:59:39 +0900

    특정폴더에 포함된 모든 JSON 파일을 객체화 하여 리스트에 담기

--- src/test/java/com/ll/exam/FileDBTest.java
+++ src/test/java/com/ll/exam/FileDBTest.java
@@ -17,6 +17,33 @@ public class FileDBTest {
         Util.deleteDir("test_data");
         Util.mkdir("test_data");
     }
+    @Test
+    void 특정_폴더에_들어있는_JSON_파일들만_모아서_객체화_시킨_후_리스트에_담기() {
+        WiseSaying wiseSaying1 = new WiseSaying(1, "내 사전에 불가능은 없다.", "나폴레옹");
+        WiseSaying wiseSaying2 = new WiseSaying(2, "나의 죽음을 적들에게 알리지 마라.", "이순신");
+
+        Util.saveToFile("test_data/1.json", wiseSaying1.toJson());
+        Util.saveToFile("test_data/2.json", wiseSaying2.toJson());
+        Util.saveNumberToFile("test_data/last_id.txt", 2);
+
+        List<String> fileNames =
+                Util.getFileNamesFromDir("test_data")
+                        .stream() // 리스트를 스트림으로 변환
+                        .filter(fileName -> fileName.endsWith(".json")) // 파일명이 .json 으로 끝나지 않는 것들은 모두 버림
+                        .collect(Collectors.toList()); // 스트림을 다시 리스트로 변환
+
+        List<WiseSaying> wiseSayings = new ArrayList<>();
+        for (String fileName : fileNames) {
+            String rs = Util.readFromFile("test_data/" + fileName);
+            Map<String, Object> map = Util.jsonToMap(rs);
+            WiseSaying wiseSaying = new WiseSaying(map);
+            wiseSayings.add(wiseSaying);
+        }
+
+        assertEquals(wiseSaying1, wiseSayings.get(0));
+        assertEquals(wiseSaying2, wiseSayings.get(1));
+    }
+
 
     @Test
     void 특정_폴더에_존재하는_모든_파일의_이름들을_가져온다() {
