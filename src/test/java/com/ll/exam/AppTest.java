27060b9a45a4c336afa4130d73e1d82214e2bad3
shinyehwan <jghisjl007@naver.com>
2022-10-17 09:44:45 +0900

    충돌부분 해결

--- src/test/java/com/ll/exam/AppTest.java
+++ src/test/java/com/ll/exam/AppTest.java
@@ -23,42 +23,6 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
  * - 입력을 문장화
  * - 출력을 문장화
  */
-<<<<<<< HEAD
-=======
-public class AppTest {
-
-    @Test
-    void 파일에_객체를_저장() {
-        Util.mkdir("test_data");
-        WiseSaying wiseSaying = new WiseSaying(1, "내 사전에 불가능은 없다.", "나폴레옹");
-        Util.saveToFile("test_data/1.json", wiseSaying.toJson());
-
-        String rs = Util.readFromFile("test_data/1.json");
-        assertEquals(wiseSaying.toJson(), rs);
-    }
-
-
-
-    @Test
-    void 파일에_내용쓰기() {
-        Util.mkdir("test_data");
-        Util.saveToFile("test_data/1.json", "내용\n내용");
-
-        String rs = Util.readFromFile("test_data/1.json");
-        assertEquals("내용\n내용", rs);
-
-    }
-
-
-    @Test
-    void 파일에_저장된_내용_가져오기() {
-        Util.mkdir("test_data");
-        Util.saveToFile("test_data/1.json", "내용");
-
-        String body = Util.readFromFile("test_data/1.json");
-        System.out.println(body);
-    }
->>>>>>> 761efac9a5366910f584efb9d2d824fbb463cade
 
 /**
  * 객체 => 한방에 => 문장
